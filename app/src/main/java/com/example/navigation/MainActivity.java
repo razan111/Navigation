8a66a9f4730273776468842d2f64cd0f27eb2259
razan111 <razan15-12966@diu.edu.bd>
2021-12-03 05:27:08 +0600

    Navigation

---
+++ app/src/main/java/com/example/navigation/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView move;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        move = findViewById(R.id.Move);
+        move.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(MainActivity.this,Second.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
